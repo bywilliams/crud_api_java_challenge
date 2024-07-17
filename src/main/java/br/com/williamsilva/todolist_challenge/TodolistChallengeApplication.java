621fe95f869635fab6d6ad0d7f10f8080d33ef2b
bywilliams <willian.seu@gmail.com>
2024-07-16 22:55:28 -0300

    Primeiro commit

---
+++ src/main/java/br/com/williamsilva/todolist_challenge/TodolistChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.williamsilva.todolist_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/williamsilva/todolist_challenge/TodolistChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.williamsilva.todolist_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
